764c0d2fbba7e13c44a7b87e0cf3e89dbc9c1915
prem kumar <144882563+raprem143@users.noreply.github.com>
2023-11-18 14:42:28 +0530

    Delete android.kt

--- android.kt
+++
@@ -1,8 +0,0 @@
-// This is my second program you can check second commit your profile
-
-fun main(){
-var x:String= "Dhanalakshmi srinivasan college of engineering and Technology"
-println("Hii kotlin")
-println(x)
-println("Hii what happend")
-}
